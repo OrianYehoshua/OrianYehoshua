6c8a3b6775c44d7c47517b89ac718c6047913a5f
OrianYehoshua <oriany1234@gmail.com>
2022-01-30 10:39:16 +0200

    start building classes

---
+++ Fridge_30.1.22/Item.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fridge_30._1._22
+{
+    class Item
+    {
+    }
+}
---
+++ Fridge_30.1.22/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fridge_30._1._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
